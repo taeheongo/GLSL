c74471d38dbaefbbe87bdb96f4c7832f0b1e381d
taeheongo <taeheon9966@icloud.com>
2021-03-04 14:25:54 +0900

    simplified sincos

--- src/shaping-functions/sincos/cos.glsl
+++ src/shaping-functions/sincos/cos.glsl
@@ -18,7 +18,7 @@ float plot(vec2 st,float pct){
 void main(){
     vec2 st=gl_FragCoord.xy/u_resolution;
     
-    float y=.5*cos((st.x)*PI*4.)+.5;
+    float y=.5*cos((st.x+u_time)*PI*4.)+.5;
     // st.x는 0~1사이의 값이므로 주기가 2pi인 cos그래프의 주기를 1로 만들어야 한다.
     // st.x에 2.0*pi를 해서 주기를 1로 만든다.
     // 0.5로 scale down을하고 +0.5만큼 평행이동한다.
--- src/shaping-functions/sincos/sin.glsl
+++ src/shaping-functions/sincos/sin.glsl
@@ -8,24 +8,23 @@ uniform vec2 u_resolution;
 uniform vec2 u_mouse;
 uniform float u_time;
 
-float plot(vec2 st,float pct){
-    float span=.05;
-    
-    return smoothstep(pct-span,pct,st.y)-
-    smoothstep(pct,pct+span,st.y);
-}
-
 void main(){
     vec2 st=gl_FragCoord.xy/u_resolution;
     
-    float y=.4*sin(st.x*PI*2.)+.5;
+    // float y=.5*sin((st.x)*PI*2.)+.5; // 그래프 원형
+    float y=.5*sin((st.x+u_time)*PI*2.)+.5;
+    
     // st.x는 0~1사이의 값이므로 주기가 2pi인 sin그래프의 주기를 1로 만들어야 한다.
     // st.x에 2.0*pi를 해서 주기를 1로 만든다.
-    // 0.5로 scale down을하고 +0.5만큼 평행이동한다.
+    // 0.5로 scale down을하고 y축방향으로 +0.5만큼 평행이동한다.
+    // 그래프를 왼쪽을 보내려면 x축 방향으로 -u_time만큼 이동한다.
     
-    vec3 color=vec3(0.,0.,0.);
-    float pct=plot(st,y);
-    color=(1.-pct)*color+pct*vec3(0.,1.,0.);
+    vec3 color;
     
+    if(y-.01<=st.y&&st.y<=y+.01){
+        color=vec3(0.,0.,0.);
+    }else{
+        color=vec3(1.,1.,1.);
+    }
     gl_FragColor=vec4(color,1.);
 }
